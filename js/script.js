93a10fc510e29290311c088a8cb7a5c856a51df4
sear93 <egor.rajewski@gmail.com>
2019-10-18 01:33:19 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(function () {
+	$('.toggles button').click(function () {
+		var get_id = this.id;
+		var get_current = $('.posts .' + get_id);
+
+		$('.post').not(get_current).hide(500);
+		get_current.show(500);
+	});
+
+	$('#all').click(function () {
+		$('.post').show(500);
+	});
+});
+
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1500,
+	autoplayHoverPause:true,
+  });
+});
+
+
+if ($(window).width() < 420){
+	$(".owl-carousel").owlCarousel({
+  	items:2,
+  	autoplay:true,
+    autoplayTimeout:1500,
+  });
+}
